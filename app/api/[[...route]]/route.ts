9f65da062e4a8265806174ec431a022754bdc570
Zubbair <zubbairhoque@gmail.com>
2025-02-24 19:59:26 +0000

    updates

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,51 +1,64 @@
-import { Hono } from 'hono'
-import { handle } from 'hono/vercel'
+import { Hono } from 'hono';
+import { handle } from 'hono/vercel';
 import { z } from 'zod';
 import { zValidator } from '@hono/zod-validator';
 
 export const runtime = 'edge';
 
-const app = new Hono().basePath('/api')
+const app = new Hono().basePath('/api'); // Fixed the syntax for creating a new Hono instance
 
 app
   .get(
     '/hello',
     (c) => {
-    return c.json({
-      message: 'Hello Next.js!',
-  })
-})
-.get(
-  "/hello/:id", 
-  
-  zValidator(
-    'param',
-    z.object({
-      id: z.string(),
-    })
-  ),
-  (c) => {
-  const { id } = c.req.valid("param");
-  return c.json({
-    message: "hello subcriber",
-    id: id,
-  });
-});
-
-  .post(
-    "/create/:postid",
-
+      return c.json({
+        message: 'Hello Next.js!',
+      });
+    }
+  )
+  .get(
+    "/hello/:id", 
     zValidator(
       'param',
       z.object({
-        postid: z.string(),
-      }) 
+        id: z.string(),
+      })
     ),
-    (c) => {
-    return c.json({})
-  }
-);
+    async (c) => {
+      const { id } = c.req.valid("param");
+      return c.json({
+        message: "hello subscriber",
+        id: id,
+      });
+    }
+  )
+  .post(
+    "/create/:postid",
+    zValidator(
+      'json',
+      z.object({
+        title: z.string(),
+        summary: z.string(),
+      })
+    ),
+    zValidator(
+      "param",
+      z.object({
+        postId: z.string(),
+      })
+    ),
+     async (c) => {
+      const { title, summary } = c.req.valid("json");
+      const { postId } = c.req.valid("param");
+      return c.json({
+        title,
+        summary,
+        postId,
+      });
+    }
+  );
 
-export const GET = handle(app)
-export const POST = handle(app)
-export const PATCH = handle(app)
+// Export handlers for different HTTP methods
+export const GET = handle(app);
+export const POST = handle(app);
+export const PATCH = handle(app);
