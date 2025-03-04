84ca2fb42f22fae365d216e6ba9ec0e8d0cf249a
Zubbair <zubbairhoque@gmail.com>
2025-03-04 13:11:24 +0000

    fixing bugs

--- lib/hono-rpc.ts
+++ lib/hono-rpc.ts
@@ -1,6 +1,7 @@
-import { hc } from 'hono/client'
-import { AppType } from "@/app/api/[[...route]]/route"
+import { hc } from 'hono/client';
+import { AppType } from "@/app/api/[[...route]]/route";
 
-export const client = hc<AppType>(process.env.HONO_API_URl!) as { api: AppType };
+// Correct the environment variable name to match the defined variable
+export const client = hc<AppType>(process.env.HONO_API_URL!);
 
 export const api = client.api;
